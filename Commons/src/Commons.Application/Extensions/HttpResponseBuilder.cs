74d206336297d974486c23a34ab41a915e9db48a
M3110 <57352425+M3110@users.noreply.github.com>
2022-07-17 17:17:58 -0300

    Changed method BuildHttpResponse to receive OperationResponse.

--- Commons/src/Commons.Application/Extensions/HttpResponseBuilder.cs
+++ Commons/src/Commons.Application/Extensions/HttpResponseBuilder.cs
@@ -1,22 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MudRunner.Commons.DataContracts.Operation;
-using System.Net;
 
 namespace MudRunner.Suspension.Application.Extensions
 {
     /// <summary>
-    /// It is responsible to build the HTTP response.
+    /// Builds the HTTP response.
     /// </summary>
     public static class HttpResponseBuilder
     {
         /// <summary>
-        /// This method builds the HTTP response .
+        /// Builds the HTTP response.
         /// </summary>
-        /// <typeparam name="TResponseData"></typeparam>
         /// <param name="response"></param>
         /// <returns></returns>
-        public static JsonResult BuildHttpResponse<TResponseData>(this OperationResponse<TResponseData> response)
-            where TResponseData : OperationResponseData, new()
+        public static JsonResult BuildHttpResponse(this OperationResponse response)
         {
             return new JsonResult(response)
             {
