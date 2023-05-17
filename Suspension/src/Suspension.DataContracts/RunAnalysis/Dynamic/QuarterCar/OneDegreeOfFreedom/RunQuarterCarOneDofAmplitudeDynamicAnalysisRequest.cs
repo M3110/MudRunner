85e1fcb8c3d4ec91cb2ef469c27dfcf9fa91c06a
M3110 <57352425+M3110@users.noreply.github.com>
2023-05-17 19:01:10 -0300

    Added data contracts for RunQuarterCarOneDofDynamicAnalysis operations.

---
+++ Suspension/src/Suspension.DataContracts/RunAnalysis/Dynamic/QuarterCar/OneDegreeOfFreedom/RunQuarterCarOneDofAmplitudeDynamicAnalysisRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MudRunner.Suspension.DataContracts.RunAnalysis.Dynamic.HalfCar.SixDegreeOfFreedom
+{
+    /// <summary>
+    /// It represents the request content of RunQuarterCarOneDofAmplitudeDynamicAnalysis operation.
+    /// </summary>
+    public class RunQuarterCarOneDofAmplitudeDynamicAnalysisRequest : RunHalfCarSixDofGenericDynamicAnalysisRequest<List<double>> { }
+}
---
+++ Suspension/src/Suspension.DataContracts/RunAnalysis/Dynamic/QuarterCar/OneDegreeOfFreedom/RunQuarterCarOneDofDynamicAnalysisRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace MudRunner.Suspension.DataContracts.RunAnalysis.Dynamic.HalfCar.SixDegreeOfFreedom
+{
+    /// <summary>
+    /// It represents the request content of RunQuarterCarOneDofDynamicAnalysis operation.
+    /// </summary>
+    public class RunQuarterCarOneDofDynamicAnalysisRequest : RunHalfCarSixDofGenericDynamicAnalysisRequest<double> { }
+}
